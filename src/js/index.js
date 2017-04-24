374bbe8237eb76403a318dc393000408c2a9a47f
aespinet <aelluvetaa@gmail.com>
2017-04-24 10:31:32 -0400

    ReadyToUse

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+// imports
+import React from 'react';
+import { render } from 'react-dom';
+
+
+
+// content
+const App = () => (
+  <h1>React Initial Project</h1>
+);
+
+
+
+// DOM - render
+render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+module.exports = {
+ 
+  entry: path.resolve(__dirname, 'src') + '/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  }
+};
